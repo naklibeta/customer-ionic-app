7c1d0c9c4db9a3ba9c1351b434b62498aef1b436
naklibeta <89298018+hemanttaskology@users.noreply.github.com>
2021-11-10 11:53:09 +0530

    initial commit

---
+++ android/app/src/main/java/com/naklibeta/customerapp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.naklibeta.customerapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { InAppBrowser, InAppBrowserOptions } from '@ionic-native/in-app-browser/ngx';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,66 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  options: InAppBrowserOptions = {
+    location: 'no',//Or 'no'
+    hidden: 'no', //Or  'yes'
+    zoom: 'no',//Android only ,shows browser zoom controls
+    hideurlbar: 'yes',//Or 'no'
+
+  };
+
+  constructor(private iab: InAppBrowser, public alertCtrl: AlertController) {
+    this.OpenWebApp();
+  }
+
+
+  OpenWebApp() {
+    let env = this;
+    const browser = this.iab.create('https://naklibeta.com/', '_self', this.options);
+    browser.on('loadstop').subscribe(event => {
+      browser.on('exit').subscribe(() => {
+        //alert('browser has been closed');
+        env.presentAlertConfirm()
+
+      }, err => {
+        alert('err' + err);
+        console.error(err);
+      });
+    });
+
+  }
+
+
+  async presentAlertConfirm() {
+    let env = this;
+    const alert = await this.alertCtrl.create({
+      //cssClass: 'my-custom-class',
+      header: 'Exit App!',
+      message: 'Are you sure, do you want to exit?',
+      buttons: [
+        {
+          text: 'No',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            env.OpenWebApp();
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Yes',
+          handler: () => {
+            navigator['app'].exitApp();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+
+
+
+
 
 }
